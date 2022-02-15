f07d4d10b254be890dcd7093c24a6baa180361ce
syuyakado <98036395+syuyakado@users.noreply.github.com>
2022-02-15 19:04:07 +0900

    Add files via upload

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book)
+    else
+      @books=Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   flash[:notice] = "Book was successfully destoyed."
+   redirect_to '/books'
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:body,:title)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+root to: 'homes#top'
+resources :books
+delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+patch 'books/:id' => 'books#update', as: 'update_book'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
